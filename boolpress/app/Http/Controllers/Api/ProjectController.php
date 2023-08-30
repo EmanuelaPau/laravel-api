a525e24496913d28893a73a37327f63d4c49f828
EmanuelaPau <uapame@gmail.com>
2023-08-30 17:28:28 +0200

    Add eager call

--- boolpress/app/Http/Controllers/Api/ProjectController.php
+++ boolpress/app/Http/Controllers/Api/ProjectController.php
@@ -11,7 +11,7 @@ class ProjectController extends Controller
     //
     public function index()
     {
-        $projects = Post::paginate(15);
+        $projects = Post::with('type', 'technologies')->paginate(15);
         return response()
             ->json([
                 'success' => true,
