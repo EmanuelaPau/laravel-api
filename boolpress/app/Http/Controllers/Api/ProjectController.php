05feb7072562a4e13c4a24a7ee3305067a7c83ce
EmanuelaPau <uapame@gmail.com>
2023-08-30 14:48:59 +0200

    Add api project controller

--- boolpress/app/Http/Controllers/Api/ProjectController.php
+++ boolpress/app/Http/Controllers/Api/ProjectController.php
@@ -3,9 +3,19 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Admin\Post;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
     //
+    public function index()
+    {
+        $projects = Post::all();
+        return response()
+            ->json([
+                'success' => true,
+                'results' => $projects,
+            ]);
+    }
 }
